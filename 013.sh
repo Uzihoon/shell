65ebd36c40e0bb9707f1c31269cb746e45e1b1fd
uzihoon <Uzihoon.dev@gmail.com>
2020-04-08 12:26:58 +0000

    013

---
+++ 013.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 013 HTML 파일에서 특정 속성값 얻기
+
+quote="[\"']"
+match="[^\"']*"
+
+while read line
+do
+	href=$(expr "$line": ".*href=${quote}\(${match}\)${quote}.*")
+	if [ $? -eq 0 ]; then
+		echo $href
+	fi
+done < index.html
